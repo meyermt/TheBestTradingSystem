4940a3c0054e27427199df92e2c16008b92f0b03
aninhabibia <anabeatriz@uchicago.edu>
2017-05-28 16:25:17 -0500

    Trader v1

---
+++ trader/src/main/java/com/vam/TradeFrame.java
@@ -0,0 +1,83 @@
+package com.vam;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * Created by ana_b on 5/12/2017.
+ */
+public class TradeFrame extends JFrame{
+
+    /** The window dimensions for the Frame */
+    public static final Dimension FRAME_DIM = new Dimension(800, 600);
+
+    /* The panel for the application **/
+    private TradePanel mPanel;
+
+    /* The controller for the trader**/
+    private Main mController;
+
+    //Interação do frame com o controller
+    public TradeFrame(Main controller) {
+
+        enableEvents(AWTEvent.WINDOW_EVENT_MASK);
+
+        //Set the game controller for the Frame
+        this.mController = controller;
+
+        try {
+            // Try to initialize the panel
+            initPanel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Initialize the frame
+        this.setTitle("Global Trader");
+        this.setSize(FRAME_DIM);
+        this.setFocusable(true);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+    }
+
+
+    /**
+     * Draws the current information to the window
+     */
+    public void draw() {
+        this.mPanel.repaint();
+    }
+    private void initPanel() throws Exception {
+
+        //Set the layout for the panel
+        JPanel contentPane = (JPanel) this.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        //Create a new GamePanel for the controller
+        this.mPanel = new TradePanel();
+
+        //Let the controller be the listener for the all actions that happen on the game panel
+//        mPanel.addKeyListener(this.mController);
+// Controller lida com todos eventos
+
+        this.addKeyListener(this.mController);
+
+        //Add the panel to the window's content panel.
+        contentPane.add(mPanel);
+    }
+
+    public void setmPanel(int tradeState) {
+        mPanel.setState(tradeState);
+    }
+
+    @Override
+    protected void processWindowEvent(WindowEvent e) {
+        super.processWindowEvent(e);
+        //Exit the game if the window closed button is closed.
+        if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+            System.exit(0);
+        }
+    }
+
+}
