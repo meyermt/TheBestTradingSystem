4f1dd186c618efbfae36ae6e1e885f70464720ce
aninhabibia <anabeatriz@uchicago.edu>
2017-06-05 20:57:01 -0500

    Ports

--- trader/src/main/java/com/vam/Main.java
+++ trader/src/main/java/com/vam/Main.java
@@ -1,12 +1,15 @@
 package com.vam;
 
 import com.vam.client.server.AdminListener;
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by michaelmeyer on 5/1/17.
@@ -19,11 +22,45 @@ public class Main implements Runnable {
     public static void main(String[] args) {
         System.out.println("Hey you're running the trader!");
         logger.info("You should see me log for trader!");
-        Main trader = new Main();
+        //Manual
+        Map<String, Integer> ports = loadPortOpts(args);
+        for (Integer port : ports.values()) {
+            Main trader = new Main(port);
+        }
+
+        //Automatic
+        
+
+
     }
 
-    public Main() {
-        TradeFrame frame = new TradeFrame(this);
+    private static Map<String, Integer> loadPortOpts(String[] args) {
+        Options options = new Options();
+        Option traderOpt = new Option("tp", TRADER_PORT, true, "TraderAdminRequest listening port to run on");
+        //Option peerOpt = new Option("pp", PEER_PORT, true, "Peer listening port to run on");
+        traderOpt.setRequired(true);
+        peerOpt.setRequired(true);
+        options.addOption(traderOpt);
+        options.addOption(peerOpt);
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+        try {
+            cmd = parser.parse(options, args);
+            Map<String, Integer> ports = new HashMap<>();
+            Integer trader = Integer.parseInt(cmd.getOptionValue(TRADER_PORT));
+            Integer peer = Integer.parseInt(cmd.getOptionValue(PEER_PORT));
+            ports.put(TRADER_PORT, trader);
+            ports.put(PEER_PORT, peer);
+            return ports;
+        } catch (ParseException e) {
+            formatter.printHelp("admin server help", options);
+            throw new RuntimeException("Unable to read arguments, see help.");
+        }
+    }
+
+    public Main(int port1, int port2) {
+        TradeFrame frame = new TradeFrame(this, port1, port2);
     }
 
 
